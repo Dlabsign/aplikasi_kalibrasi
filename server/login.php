21f77bf27f71349a14ab04bd7af7a22b4236b91e
Dlabsign <danielkurnip02@gmail.com>
2024-12-09 10:15:17 +0700

    login_api

--- server/login.php
+++ server/login.php
@@ -1,34 +1,16 @@
 <?php
-// Mengatur koneksi ke database MySQL
-$servername = "localhost";
-$username = "root";  // ganti dengan username MySQL Anda
-$password = "";  // ganti dengan password MySQL Anda
-$dbname = "kalibrasi";  // ganti dengan nama database Anda
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'kalibrasi';
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Cek apakah koneksi berhasil
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+$conn = mysqli_connect($host, $username, $password, $database);
+if (!$conn) {
+    die("Koneksi ke database gagal: " . mysqli_connect_error());
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Ambil data yang dikirim melalui POST
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+$query = mysqli_query($conn, "SELECT * FROM user");
+$tampil = mysqli_fetch_all($query, MYSQLI_ASSOC);
+echo json_encode($tampil);
 
-    // Query untuk memeriksa apakah username dan password ada di database
-    $sql = "SELECT * FROM user WHERE user_username = '$username' AND user_password = '$password'";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        // Jika data ditemukan, login berhasil
-        echo json_encode(["status" => "success"]);
-    } else {
-        // Jika data tidak ditemukan, login gagal
-        echo json_encode(["status" => "failed"]);
-    }
-}
-
-$conn->close();
 ?>
